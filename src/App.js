2de965f23533a668e2966068eb89ebfaf484550f
Dmitry Lambrianov <dmitry.lambrianov@nentgroup.com>
2021-09-17 14:59:48 +0200

    add genre mapper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import contentData from "./fixtures/content.json";
-
 const allMovies = contentData._embedded['viaplay:blocks'].reduce((acc, block) => {
   const allMoviesPerBlock = block._embedded && block._embedded['viaplay:products'];
   if (!allMoviesPerBlock) {
@@ -13,24 +13,127 @@ const allMovies = contentData._embedded['viaplay:blocks'].reduce((acc, block) =>
   })
   return acc;
 }, new Map());
-
 const allMoviesArray = [...allMovies].map(([name, value]) => ({ name, value }));
+
+const allGenres = contentData._links['viaplay:categoryFilters'].reduce((acc, cat) => {
+  let translation = '';
+  switch (cat.title) {
+    case "Action":
+      translation = 'action';
+      break;
+    case "Animation":
+      translation = 'animation';
+      break;
+    case 'Barnfilm':
+      translation = 'kids';
+      break;
+    case 'Dokumentärfilm':
+      translation = 'documentary';
+      break;
+    case 'Drama':
+      translation = 'drama';
+      break;
+    case 'Familjefilm':
+      translation = 'family';
+      break;
+    case 'Komedi':
+      translation = 'comedy';
+      break;
+    case 'Kriminaldrama':
+      translation = 'crime';
+      break;
+    case 'Romantik':
+      translation = 'romance';
+      break;
+    case 'Rysare':
+      translation = 'horror';
+      break;
+    case 'Science fiction':
+      translation = 'science fiction';
+      break;
+    case 'Thriller':
+      translation = 'thriller';
+      break;
+    case 'Äventyr':
+      translation = 'adventure';
+      break;
+    default:
+      translation = '';
+      break;
+  }
+
+  if (translation === '') {
+    return acc;
+  } else {
+    acc[translation] = cat.title
+  }
+
+
+  return acc;
+}, {});
+
+const MovieBlock = styled.div`
+  display: flex;
+  width: 800px;
+  margin: auto;
+  flex-wrap: wrap;
+  justify-content: center;
+  color: red;
+`;
+
+const Movie = styled.p`
+  margin: 5px;
+`;
+
+const Genre = styled.h3`
+  color: #ffffff;
+`;
+
+const SpeechButton = styled.button`
+  background-color: ${props => props.active ? "red" : "#ff5bb0"};
+  border-radius: 10px;
+  height: 60px;
+  width: 150px;
+  border:1px solid #851467;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:15px;
+  font-weight:bold;
+  padding:6px 24px;
+  text-decoration:none;
+  text-shadow:0px 1px 0px #c70067;
+  margin-bottom: 20px;
+`;
+
+const ImagePlaceholder = styled.img``;
+const ImagePlaceholderLogo = styled.img`
+  display: block;
+`;
 
 const enableSpeechRecognition = () => {
   const genres = [
-    "thriller",
-    "science fiction",
-    "drama",
-    "animation",
-    "action",
-    "comedy"
+    'action',
+    'animation',
+    'kids',
+    'documentary',
+    'drama',
+    'family',
+    'comedy',
+    'crime',
+    'romance',
+    'horror',
+    'science fiction',
+    'thriller',
+    'adventure'
   ];
   const grammar =
     "#JSGF V1.0; grammar genres; public <genre> = " + genres.join(" | ") + " ;";
 
   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   const SpeechGrammarList = window.window.SpeechGrammarList || window.webkitSpeechGrammarList;
-  const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+  // const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
   const recognition = new SpeechRecognition();
   const speechRecognitionList = new SpeechGrammarList();
@@ -42,27 +145,14 @@ const enableSpeechRecognition = () => {
   recognition.interimResults = false;
   recognition.maxAlternatives = 1;
 
-  recognition.onspeechend = function() {
-    recognition.stop();
-  }
-
   return recognition;
 };
 
 const getMoviesPerTranscript = (transcript) => {
+  const requestedGenre = allGenres[transcript];
   const newMovies = allMoviesArray.filter(movie => {
-    const movieGenres = movie.value._links['viaplay:genres'].map(genre => {
-      let title = '';
-      // TODO: make a config with all genres
-      if (genre.title === 'Komedi') {
-        title = 'comedy'
-      } else {
-        title = genre.title
-      }
-      return title.toLowerCase();
-    });
-    
-    if (movieGenres.includes(transcript)) {
+    const movieGenres = movie.value._links['viaplay:genres'].map(genre => genre.title);
+    if (movieGenres.includes(requestedGenre)) {
       return movie;
     }
   });
@@ -73,9 +163,10 @@ const getMoviesPerTranscript = (transcript) => {
 function App() {
   const [recognition, setSpeech] = useState(null);
   const [movies, setMovies] = useState([]);
+  const [isSpeechActive, setSpeechActive] = useState(false);
+  const [genre, setGenre] = useState('');
 
-  const renderContent = (event) => {
-    const transcript = event.results[0][0].transcript;
+  const renderContent = (transcript) => {
     const newMovies = getMoviesPerTranscript(transcript)
     setMovies(newMovies)
   }
@@ -86,13 +177,25 @@ function App() {
 
   return (
     <div className="App" >
-      <button onClick={() => {
+      <ImagePlaceholderLogo src="https://viaplay-web-frontend-assets.mtg-api.com/frontend-2021091627423/images/header-logo.png" />
+      <SpeechButton active={isSpeechActive} onClick={() => {
+        setSpeechActive(true);
         recognition.start()
         recognition.onresult = function(event) {
-          renderContent(event)
+          const transcript = event.results[0][0].transcript;
+          renderContent(transcript)
+          setGenre(transcript);
+          setSpeechActive(false)
         }
-        }}>Say what to watch!</button>
-        {movies.map(movie => <p>{movie.name}</p>)}
+        recognition.onspeechend = function() {
+          recognition.stop();
+          setSpeechActive(false);
+        }
+        }}>Speak out your desired title!</SpeechButton>
+        <Genre>{genre.toUpperCase()}</Genre>
+        <MovieBlock>{movies.map(movie => <Movie key={movie.name}>
+          <ImagePlaceholder src={movie.value.content.images.boxart.url}></ImagePlaceholder>
+        </Movie>)}</MovieBlock>
     </div>
   );
 }
