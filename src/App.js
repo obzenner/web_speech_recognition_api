e2a1ddde7eaba6106515d48d53e0bfa6d0357199
Dmitry Lambrianov <dmitry.lambrianov@nentgroup.com>
2021-09-17 14:13:11 +0200

    text based recognition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from 'react';
+
+import contentData from "./fixtures/content.json";
+
+const allMovies = contentData._embedded['viaplay:blocks'].reduce((acc, block) => {
+  const allMoviesPerBlock = block._embedded && block._embedded['viaplay:products'];
+  if (!allMoviesPerBlock) {
+    return acc;
+  }
+  allMoviesPerBlock.forEach(movie => {
+    acc.set(movie._links.self.title, movie);
+  })
+  return acc;
+}, new Map());
+
+const allMoviesArray = [...allMovies].map(([name, value]) => ({ name, value }));
+
+const enableSpeechRecognition = () => {
+  const genres = [
+    "thriller",
+    "science fiction",
+    "drama",
+    "animation",
+    "action",
+    "comedy"
+  ];
+  const grammar =
+    "#JSGF V1.0; grammar genres; public <genre> = " + genres.join(" | ") + " ;";
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const SpeechGrammarList = window.window.SpeechGrammarList || window.webkitSpeechGrammarList;
+  const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+  const recognition = new SpeechRecognition();
+  const speechRecognitionList = new SpeechGrammarList();
+  speechRecognitionList.addFromString(grammar, 1);
+
+  recognition.grammars = speechRecognitionList;
+  recognition.continuous = false;
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.onspeechend = function() {
+    recognition.stop();
+  }
+
+  return recognition;
+};
+
+const getMoviesPerTranscript = (transcript) => {
+  const newMovies = allMoviesArray.filter(movie => {
+    const movieGenres = movie.value._links['viaplay:genres'].map(genre => {
+      let title = '';
+      // TODO: make a config with all genres
+      if (genre.title === 'Komedi') {
+        title = 'comedy'
+      } else {
+        title = genre.title
+      }
+      return title.toLowerCase();
+    });
+    
+    if (movieGenres.includes(transcript)) {
+      return movie;
+    }
+  });
+
+  return newMovies;
+};
 
 function App() {
+  const [recognition, setSpeech] = useState(null);
+  const [movies, setMovies] = useState([]);
+
+  const renderContent = (event) => {
+    const transcript = event.results[0][0].transcript;
+    const newMovies = getMoviesPerTranscript(transcript)
+    setMovies(newMovies)
+  }
+
+  useEffect(() => {
+   setSpeech(enableSpeechRecognition());
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={() => {
+        recognition.start()
+        recognition.onresult = function(event) {
+          renderContent(event)
+        }
+        }}>Say what to watch!</button>
+        {movies.map(movie => <p>{movie.name}</p>)}
     </div>
   );
 }
